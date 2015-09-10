677015dc59f154b4ba0448d916c2bad87a372c5c
Sara-Monica <sfsmc6@gmail.com>
2015-09-09 19:56:15 -0700

    CelsiusToFahrenheit

---
+++ CelsiusToFahrenheit/CelsiusToFahrenheit.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class CelsiusToFahrenheit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CelsiusToFahrenheit
+{
+    public static void main(String[] args)
+    {
+        double celsius = 0.0;
+        double fahrenheit;
+        fahrenheit= celsius*1.8+32;
+        System.out.println (fahrenheit);
+    }
+}
