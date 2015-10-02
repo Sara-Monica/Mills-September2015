e4b1215e64fc91c6056b7834e391697d3cefaafe
Sara-Monica <sfsmc6@gmail.com>
2015-10-01 20:33:47 -0700

    ASCIIArt
    Its an Elephant. It doesn't get better than that.

---
+++ ASCIIArt/ASCIIArt.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class ASCIIArt here.
+ * 
+ * @Sara Cisneros 
+ * @October.1.2015
+ */
+public class ASCIIArt
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    public static void elephant()
+    {
+        System.out.println ("     ,__________,\"\"\"\"\"\"\",");
+        System.out.println ("   ,\"        ,/   |      \\    ");
+        System.out.println ("   |         /    |     o \\   ");
+        System.out.println ("   |        '     |      > |   __");
+        System.out.println ("  /|        \",,,,' _____|  \\__/  /");
+        System.out.println (",/ |      ____     |     \\______/ ");
+        System.out.println ("'\" |     |    |    |      ");
+        System.out.println ("   |_____|    |____|      ");
+    } 
+}
