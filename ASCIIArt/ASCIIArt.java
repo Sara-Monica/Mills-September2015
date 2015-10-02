579c1082ed7346dc35ce02f5f66e2f33ea48900d
Sara-Monica <sfsmc6@gmail.com>
2015-10-02 08:54:49 -0700

    There are now two elephants. It did get better.

--- ASCIIArt/ASCIIArt.java
+++ ASCIIArt/ASCIIArt.java
@@ -11,15 +11,28 @@ public class ASCIIArt
     {
         
     }
-    public static void elephant()
+    public static void elephantFacingRight()
     {
-        System.out.println ("     ,__________,\"\"\"\"\"\"\",");
-        System.out.println ("   ,\"        ,/   |      \\    ");
-        System.out.println ("   |         /    |     o \\   ");
-        System.out.println ("   |        '     |      > |   __");
+        System.out.println ("     ,__________,\"\"\"\"\"\"\",         ");
+        System.out.println ("   ,\"        ,/   |      \\        ");
+        System.out.println ("   |         /    |     o \\       ");
+        System.out.println ("   |        '     |      > |   __ ");
         System.out.println ("  /|        \",,,,' _____|  \\__/  /");
         System.out.println (",/ |      ____     |     \\______/ ");
         System.out.println ("'\" |     |    |    |      ");
         System.out.println ("   |_____|    |____|      ");
-    } 
+    }
+    public static void elephantFacingLeft()
+    {
+        System.out.println ("         ,\"\"\"\"\"\"\",__________,     ");
+        System.out.println ("        /       |   \\,       \",   ");
+        System.out.println ("       / o      |    \\        |   ");
+        System.out.println (" __   | <       |     '       |   ");
+        System.out.println ("\\  \\__/  |_____  ',,,,\"       |\\, ");
+        System.out.println (" \\______/     |     ____      | \\");
+        System.out.println ("              |    |    |     | \"'");
+        System.out.println ("              |____|    |_____|   ");
+    }
+    
+   
 }
